0624175387490dc973ba1d6dc3edc6cf7e0c8b84
Neuron <dev.bhardwaj851@gmail.com>
2021-01-04 18:36:06 +0530

    Adding Burger Compoonet

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Aux  from '../../hoc/Auxilary';
+import classes from './Layout.module.css';
+
+const layout = (props) => {
+     return (
+          <Aux>
+               <div>ToolBar , SideBar, BackDrop</div>
+               <main className={classes.Content}>
+                   {props.children} 
+               </main>
+          </Aux>
+     );
+}
+
+export default layout;
